15ceb4b6062864bf41324b8cd60ec4b291ec1b8d
DDusy <dkdnb50@gmail.com>
2022-04-26 17:25:38 +0900

    Backjoon 17144
    1.미리 루트 셋팅
    2.미세먼지 확산
    3.저장해 놓은 루트대로 데이터 이동

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -27,89 +27,167 @@
 #include <openssl//pem.h>
 #include <openssl//err.h>
 
+int r,c,t;
+int origin[51][51];
+int temp[51][51];
+int dx[]=  { 0,-1,0,1};
+int dy[] = { 1,0,-1,0};
+int dx2[] = { 0,1,0,-1 };
+int dy2[] = { 1,0,-1,0 };
 
-using namespace std;
+pair<int, int> Purifier1={-1,-1},Purifier2= { -1,-1 };
 
-int m, n, cnt = 0, mx, big;
-int board[51][51] = { 0, }, visited[51][51] = { 0, }, Rooms[51];
+queue<pair<int,int>> q;
+vector<pair<int,int>>  route1,route2;
 
-int dx[] = { 0,-1,0,1 };
-int dy[] = { -1,0,1,0};
+int res=0;
 
-int dfs(int x, int y, int cnt)
+void Dust()
 {
-	if (visited[x][y]) return 0;
-	visited[x][y] = cnt;
+	memset(&temp,0,sizeof(temp));
 
-	int res = 1;
-
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < r; ++i)
 	{
-		if (!(board[x][y] & (1 << i)))
+		for (int j = 0; j < c; ++j)
 		{
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-
-			if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-
-			res += dfs(nx, ny, cnt);
+			if(-1!=origin[i][j] && origin[i][j])	// 다 집어넣기
+					q.push({i,j});
 		}
 	}
 
-	return res;
+	while (!q.empty())
+	{
+
+		int _x=q.front().first;
+		int _y=q.front().second;
+		q.pop();
+
+		int spread = origin[_x][_y] / 5;
+
+		for (int i = 0; i < 4; ++i)
+		{
+			int nx = _x + dx[i];
+			int ny = _y + dy[i];
+
+			if (nx < 0 || ny < 0 || nx >= r || ny >= c || -1 == origin[nx][ny]) continue;
+
+			temp[nx][ny] += spread;
+			origin[_x][_y] -= spread;
+		}
+	}
+	
+
+	for (int i = 0; i < r; ++i)
+	{
+		for (int j = 0; j < c; ++j)
+		{
+			origin[i][j]+=temp[i][j];
+
+		}
+	}
+}
+
+void SetRoute(pair<int,int> _start,vector<pair<int,int>>& _route,int _dx[], int _dy[])
+{
+	
+	int dirX =_start.first;
+	int dirY = _start.second;
+
+	int idx=0;
+
+	while (true)
+	{
+		if(idx==4)
+		 break;
+
+		int nx = dirX + _dx[idx];
+		int ny = dirY + _dy[idx];
+
+		if (nx < 0 || ny < 0 || nx >= r || ny >= c || -1 == origin[nx][ny] )
+		{
+			++idx;
+			continue;
+		}
+
+		dirX=nx;
+		dirY=ny;
+
+		_route.push_back({dirX,dirY});
+	}
+}
+
+void AirClean(vector<pair<int, int>>& _route)
+{
+	for (int i = _route.size() - 1; i > 0; --i)
+	{
+		origin[_route[i].first][_route[i].second] = origin[_route[i-1].first][_route[i-1].second];
+	}
+	
+	origin[_route[0].first][_route[0].second]= 0;
 }
 
 int main()
 {
-	cin >> m >> n;
+	cin>>r>>c>>t;
 
-	for (int i = 0; i < n; ++i)
+	for (int i = 0; i < r; ++i)
 	{
-		for (int j = 0; j < m; ++j)
+		for (int j = 0; j < c; ++j)
 		{
-			cin >> board[i][j];
+			cin>>origin[i][j];
+
+			if(-1==origin[i][j])
+				{
+					if(Purifier1.first==-1 )
+						{
+							Purifier1={i,j};
+							SetRoute(Purifier1, route1, dx, dy);
+						}
+					else
+						{
+							Purifier2 = { i,j };
+							SetRoute(Purifier2, route2, dx2, dy2);
+						}
+				}	
+
 		}
 	}
 
-	for (int i = 0; i < n; ++i)
+	
+
+	while (t--)
 	{
-		for (int j = 0; j < m; ++j)
+		Dust();
+
+		cout<< "확산"<<endl;
+
+		for (int i = 0; i < r; ++i)
 		{
-			if (!visited[i][j])
-			{
-				++cnt;
-				Rooms[cnt] = dfs(i, j, cnt);
-				mx = max(mx, Rooms[cnt]);
-			}
+			for (int j = 0; j < c; ++j)
+				cout<<origin[i][j]<<" ";
+				cout<<endl;
 		}
-	}
+		AirClean(route1);
+		AirClean(route2);
 
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = 0; j < m; ++j)
+
+		cout << "결과" << endl;
+
+		for (int i = 0; i < r; ++i)
 		{
-			int cur = visited[i][j];
-			if (i + 1 < n)
-			{
-				int tmp = visited[i + 1][j];
-
-				if(cur!=tmp)
-				big = max(big, Rooms[cur] + Rooms[tmp]);
-
-			}
-
-			if (j + 1 < m)
-			{
-				
-				int tmp = visited[i][j + 1];
-
-				if (cur != tmp)
-				big = max(big, Rooms[cur] + Rooms[tmp]);
-			}
+			for (int j = 0; j < c; ++j)
+				cout << origin[i][j] << " ";
+			cout << endl;
 		}
 	}
 
 
-	cout << cnt << "\n" << mx << "\n" << big << "\n";
+	for (int i = 0; i < r; ++i)
+	{
+		for(int j=0;j<c;++j)
+		 res+=origin[i][j];
+	}
+
+	cout<<res+2<<"\n";
 	return 0;
 }
