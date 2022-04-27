e9ce8072ec461e1fe07debb921e4b2f660b31299
DDusy <dkdnb50@gmail.com>
2022-04-28 01:39:13 +0900

    Backjoon 14889 스타트와 링크
    1.모든 경우의 수 탐색
    2. i & (1<<n) 비트 마스킹으로 팀찾기
    3. 두팀 공격력 계산하기 (행열이 같을 때를 빼곤 계산 => n/2)
    4. 최소값비교

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -27,167 +27,63 @@
 #include <openssl//pem.h>
 #include <openssl//err.h>
 
-int r,c,t;
-int origin[51][51];
-int temp[51][51];
-int dx[]=  { 0,-1,0,1};
-int dy[] = { 1,0,-1,0};
-int dx2[] = { 0,1,0,-1 };
-int dy2[] = { 1,0,-1,0 };
 
-pair<int, int> Purifier1={-1,-1},Purifier2= { -1,-1 };
+int n,res=987654321;
 
-queue<pair<int,int>> q;
-vector<pair<int,int>>  route1,route2;
+int s[20][20]={0,};
+int visited[20]={0,};
 
-int res=0;
-
-void Dust()
-{
-	memset(&temp,0,sizeof(temp));
-
-	for (int i = 0; i < r; ++i)
-	{
-		for (int j = 0; j < c; ++j)
-		{
-			if(-1!=origin[i][j] && origin[i][j])	// 다 집어넣기
-					q.push({i,j});
-		}
-	}
-
-	while (!q.empty())
-	{
-
-		int _x=q.front().first;
-		int _y=q.front().second;
-		q.pop();
-
-		int spread = origin[_x][_y] / 5;
-
-		for (int i = 0; i < 4; ++i)
-		{
-			int nx = _x + dx[i];
-			int ny = _y + dy[i];
-
-			if (nx < 0 || ny < 0 || nx >= r || ny >= c || -1 == origin[nx][ny]) continue;
-
-			temp[nx][ny] += spread;
-			origin[_x][_y] -= spread;
-		}
-	}
-	
-
-	for (int i = 0; i < r; ++i)
-	{
-		for (int j = 0; j < c; ++j)
-		{
-			origin[i][j]+=temp[i][j];
-
-		}
-	}
-}
-
-void SetRoute(pair<int,int> _start,vector<pair<int,int>>& _route,int _dx[], int _dy[])
-{
-	
-	int dirX =_start.first;
-	int dirY = _start.second;
-
-	int idx=0;
-
-	while (true)
-	{
-		if(idx==4)
-		 break;
-
-		int nx = dirX + _dx[idx];
-		int ny = dirY + _dy[idx];
-
-		if (nx < 0 || ny < 0 || nx >= r || ny >= c || -1 == origin[nx][ny] )
-		{
-			++idx;
-			continue;
-		}
-
-		dirX=nx;
-		dirY=ny;
-
-		_route.push_back({dirX,dirY});
-	}
-}
-
-void AirClean(vector<pair<int, int>>& _route)
-{
-	for (int i = _route.size() - 1; i > 0; --i)
-	{
-		origin[_route[i].first][_route[i].second] = origin[_route[i-1].first][_route[i-1].second];
-	}
-	
-	origin[_route[0].first][_route[0].second]= 0;
-}
 
 int main()
 {
-	cin>>r>>c>>t;
+	cin >> n;
 
-	for (int i = 0; i < r; ++i)
+	for (int i = 0; i < n; ++i)
 	{
-		for (int j = 0; j < c; ++j)
+		for (int j = 0; j < n; ++j)
 		{
-			cin>>origin[i][j];
-
-			if(-1==origin[i][j])
-				{
-					if(Purifier1.first==-1 )
-						{
-							Purifier1={i,j};
-							SetRoute(Purifier1, route1, dx, dy);
-						}
-					else
-						{
-							Purifier2 = { i,j };
-							SetRoute(Purifier2, route2, dx2, dy2);
-						}
-				}	
-
+			cin >> s[i][j];
 		}
 	}
 
-	
-
-	while (t--)
+	for (int i = 0; i < (1 << n); ++i)
 	{
-		Dust();
-
-		cout<< "확산"<<endl;
-
-		for (int i = 0; i < r; ++i)
+		int cnt = 0;
+		for (int j = 0; j < n; ++j)
 		{
-			for (int j = 0; j < c; ++j)
-				cout<<origin[i][j]<<" ";
-				cout<<endl;
+			if (i & (1 << j))++cnt;
 		}
-		AirClean(route1);
-		AirClean(route2);
 
+		if (n / 2 != cnt) continue;
 
-		cout << "결과" << endl;
-
-		for (int i = 0; i < r; ++i)
+		vector<int> vTeam1, vTeam2;
+		for (int j = 0; j < n; ++j)
 		{
-			for (int j = 0; j < c; ++j)
-				cout << origin[i][j] << " ";
-			cout << endl;
+			if (i & (1 << j))
+				vTeam1.push_back(j);
+			else
+				vTeam2.push_back(j);
 		}
+
+		int t1 = 0, t2 = 0;
+		for (int j = 0; j < (n / 2); ++j)
+		{
+			for (int h = 0; h < (n / 2); ++h)
+			{
+
+				t1 += s[vTeam1[j]][vTeam1[h]];
+				t2 += s[vTeam2[j]][vTeam2[h]];
+			}
+
+		}
+
+		int diff = t1 - t2;
+		if (diff < 0) diff = -diff;
+
+		res = min(res, diff);
 	}
 
+	cout << res << '\n';
 
-	for (int i = 0; i < r; ++i)
-	{
-		for(int j=0;j<c;++j)
-		 res+=origin[i][j];
-	}
-
-	cout<<res+2<<"\n";
 	return 0;
 }
